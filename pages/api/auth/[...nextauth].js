0362c9052e4b87f27aa3c0d6b3cf80915fda0fb5
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-09-07 19:41:41 +0600

    login with oAuth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,29 @@
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import clientPromise from "../../../lib/mongodb";
+
+export default NextAuth({
+    adapter: MongoDBAdapter(clientPromise),
+    // Configure one or more authentication providers
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+        // ...add more providers here
+    ],
+    pages: {
+        signIn: "/login",
+    },
+    session: {
+        strategy: "jwt",
+    },
+    callbacks: {
+        session: async ({ token, session }) => {
+            // console.log(`JWT` , token);
+            // console.log(`Session` , session);
+            return Promise.resolve(session);
+        }
+    }
+});
---
+++ pages/login.js
@@ -0,0 +1,28 @@
+import { getProviders, signIn } from "next-auth/react";
+import React from 'react';
+import { FcGoogle } from "react-icons/fc";
+
+export default function login({ providers }) {
+
+    return (
+        <div className="flex items-center justify-center h-screen">
+            {
+                Object?.values(providers).map((provider) => (
+                    <div key={provider.name}>
+                        <button className="bg-twitterLightGrey px-5 py-3 rounded-full flex justify-center items-center" onClick={() => signIn(provider?.id)}>
+                            <FcGoogle className="w-6 h-6 mr-2" />
+                            Sign in with {provider?.name}
+                        </button>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+export async function getServerSideProps(context) {
+    const providers = await getProviders();
+    return {
+        props: providers ? { providers } : { providers: {} },
+    }
+}
